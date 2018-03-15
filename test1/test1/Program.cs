29638f3a970c241c27e2d51884fd8e434f89e512
m305-35\w401 <biyori03091@gmail.com>
2018-03-15 17:43:48 +0800

    01

---
+++ test1/test1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("╔═══════╗" + "\r\n" + "║              ║" + "\r\n" + "║    D M A     ║" + "\r\n" + "║              ║" + "\r\n" + "╚═══════╝");
+            Console.ReadLine();
+
+
+
+
+            // 宣告匯率變數與數值
+            //double rate = 0.277;
+
+            // 顯示匯率
+            //Console.WriteLine("目前日幣匯率 : " + rate);
+
+            // 接收輸入數值，存入變數
+            //Console.WriteLine("請輸入日幣金額 : ");
+            //string input = Console.ReadLine();
+
+            // 顯示結果
+            //double total = double.Parse(input) * rate;
+            // Console.WriteLine("換算成新台幣為 : " + total);
+
+            // 讓視窗停留，不會自動關閉
+            //Console.ReadLine();
+        }
+    }
+}
